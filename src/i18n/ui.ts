91cf7aacdb96e20b88c69fd95a4be4d484478380
melyuh <yuh.koba@gmail.com>
2026-06-28 01:28:35 +0900

    add: base-layout files

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const ui = {
+  nav: {
+    home: "HOME",
+    blog: "BLOG",
+    works: "WORKS",
+    profile: "PROFILE",
+  },
+  site: {
+    name: "知識を融かして、かたちにするブログ",
+    copyright: "Copyright © Melyuh",
+  },
+  footer: {
+    rss: "RSS",
+  },
+} as const;
